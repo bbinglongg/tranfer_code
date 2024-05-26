5bc5c98344c1ecbcf4186d5c2e980be9a2ef2fe9
Sam LIU <375780431@qq.com>
2024-05-26 21:11:09 +0800

    Update popup.jss

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // 设置悬浮框的样式
   floatingBox.style.position = 'fixed';
   floatingBox.style.top = '10px'; // 距离页面顶部 10px
-  floatingBox.style.left = '10px'; // 距离页面左侧 10px
+  floatingBox.style.right = '10px'; // 距离页面右侧 10px
   floatingBox.style.backgroundColor = 'rgba(255, 255, 255, 0.7)'; // 半透明白色背景
   floatingBox.style.padding = '10px';
   floatingBox.style.zIndex = '9999'; // 确保浮动框在最顶层
