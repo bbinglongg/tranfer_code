485481ac83ac2aefa161be86ff4a263c9a394ea9
Sam LIU <375780431@qq.com>
2024-07-14 12:06:49 +0800

    Create gpt_2_up_down.py

---
+++ gpt_2_up_down.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+# 读取数据
+intraday_data = pd.read_csv('Simulation_INTRADAY_DATA_HSI.csv')
+daily_data = pd.read_csv('daily.csv')
+dummay_data = pd.read_csv('Dummay_data.csv')
+
+# 提取每日涨跌符号
+def extract_daily_trend(data):
+    data['date'] = pd.to_datetime(data['date'])
+    data.set_index('date', inplace=True)
+    data['change'] = data['current'].diff()
+    data['trend'] = (data['change'] > 0).astype(int)
+    daily_trend = data.resample('D').last()['trend']
+    return daily_trend.dropna()
+
+dummy_daily_trend = extract_daily_trend(dummay_data)
+
+# 匹配时间段 
+def find_matching_period(daily_trend, historical_data):
+    historical_data['date'] = pd.to_datetime(historical_data['日期'])
+    historical_data.set_index('date', inplace=True)
+    historical_data['change'] = historical_data['收市'].diff()
+    historical_data['trend'] = (historical_data['change'] > 0).astype(int)
+
+    for start in range(len(historical_data) - len(daily_trend) + 1):
+        match = True
+        for i in range(len(daily_trend)):
+            if historical_data['trend'].iloc[start + i] != daily_trend.iloc[i]:
+                match = False
+                break
+        if match:
+            return historical_data.index[start]
+
+    return None
+
+matching_start_date = find_matching_period(dummy_daily_trend, daily_data)
+
+
+# 预测最后一周的最高值 
+def get_predicted_max_value(matching_start_date, historical_data):
+    matching_period = historical_data.loc[matching_start_date:matching_start_date + pd.DateOffset(weeks=3)]
+    next_week = historical_data.loc[matching_start_date + pd.DateOffset(weeks=3):matching_start_date + pd.DateOffset(weeks=4)]
+    return next_week['最高'].max()
+
+predicted_max_value = get_predicted_max_value(matching_start_date, daily_data)
+
+# 找出masking的规律 
+def find_masking_formula(dummay_data, intraday_data):
+    # 计算前3个星期的masking系数
+    dummy_max = dummay_data['current'].max()
+    historical_max = intraday_data['current'].max()
+    masking_coefficient = dummy_max / historical_max
+    return masking_coefficient
+
+masking_coefficient = find_masking_formula(dummay_data, intraday_data)
+
+def apply_masking_coefficient(value, coefficient):
+    return value * coefficient
+
+adjusted_max_value = apply_masking_coefficient(predicted_max_value, masking_coefficient)
+
